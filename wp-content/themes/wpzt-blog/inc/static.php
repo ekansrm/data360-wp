cadd010ab0ddd09a48e8175edd99516c3a3e70b8
ekansrm <ekansrm0001@163.com>
2022-07-17 20:53:40 +0800

    + 恢复 Jquery 的注册

--- wp-content/themes/wpzt-blog/inc/static.php
+++ wp-content/themes/wpzt-blog/inc/static.php
@@ -17,11 +17,11 @@ function reg_static(){
 			wp_register_script($filename,WPZT_JS.str_replace($jspath,'',$v));
 		}
 	}
-//	wp_register_script('jq21','https://s2.pstatp.com/cdn/expire-1-M/jquery/2.1.1/jquery.min.js');
+    wp_register_script('jq21','https://s2.pstatp.com/cdn/expire-1-M/jquery/2.1.1/jquery.min.js');
 	wp_register_script("bootstrap","https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js");
 	wp_register_script('wangeditor','https://s1.pstatp.com/cdn/expire-1-M/wangEditor/3.1.1/wangEditor.min.js');
 	wp_register_script('plyr',"https://s0.pstatp.com/cdn/expire-1-M/plyr/3.5.4/plyr.min.js");
-//	wp_register_script('sweetalert','https://s3.pstatp.com/cdn/expire-1-M/sweetalert/2.1.0/sweetalert.min.js');
+    wp_register_script('sweetalert','https://s3.pstatp.com/cdn/expire-1-M/sweetalert/2.1.0/sweetalert.min.js');
 	$csspath=WPZT_DIR.'/static/css';
 	
 	if(empty(Cache::get('allcssfile'))){
